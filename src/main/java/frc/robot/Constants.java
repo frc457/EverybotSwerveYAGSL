82cec8c3d90e4b2a55da78792e701aa6f633e59d
peterpalaciosjr <peterpalaciosjr9@gmail.com>
2025-02-28 20:57:43 -0600

    Change Coral Out speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,7 +52,8 @@ public final class Constants
     public static final int ROLLER_MOTOR_ID = 2;
     public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
     public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ROLLER_CORAL_OUT = .65;
+    public static final double ROLLER_CORAL_OUT = .60
+  ;
     public static final double ROLLER_ALGAE_IN = -1.0;
     public static final double ROLLER_ALGAE_OUT = 0.55;
     public static final double ROLLER_CORAL_STACK = 1;
